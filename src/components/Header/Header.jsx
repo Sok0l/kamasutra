a850c731e107014f7723f6026c44620759966940
Sok0l <1levsokolov22@gmail.com>
2023-01-13 17:05:27 +0300

    add: lacal state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import s from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <header className={s.header}>
       <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo" />
+
       <div className={s.loginBlock}>
         {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
       </div>
