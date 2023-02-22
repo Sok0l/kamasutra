178c74731ed5c4be6f5bc55362b248e2bc5ed48b
Sok0l <1levsokolov22@gmail.com>
2023-02-22 17:21:39 +0300

    end course

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,54 +5,83 @@ import { requiredField } from "../../utils/validators";
 import { connect } from "react-redux";
 import { login } from "../../redux/auth-reducer";
 import { Navigate } from "react-router-dom";
-import s from "../../common/Preloader/FormsControls/FormControls.module.css";
 
 const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Field
-          validate={[requiredField]}
-          placeholder={"Email"}
-          name={"email"}
-          component={FormControl}
-          types="input"
-        />
-      </div>
-      <div>
-        <Field
-          validate={[requiredField]}
-          placeholder={"Password"}
-          name={"password"}
-          component={FormControl}
-          types="input"
-          type={"password"}
-        />
-      </div>
-      <div>
-        <Field
-          component={FormControl}
-          name={"rememberMe"}
-          type={"checkbox"}
-          types="input"
-        />
-        remember me
-      </div>
-      {captchaUrl && <img src={captchaUrl} alt="captcha" />}
-      {captchaUrl && (
-        <Field
-          validate={[requiredField]}
-          placeholder={"Symbols from image"}
-          name={"captcha"}
-          component={FormControl}
-          types="input"
-        />
-      )}
-      {error && <div className={s.formSummoryError}>{error}</div>}
-      <div>
-        <button>Login</button>
-      </div>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            E-mail
+          </label>
+          <Field
+            validate={[requiredField]}
+            placeholder={"Email"}
+            name={"email"}
+            component={FormControl}
+            types="input"
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+
+          <div id="emailHelp" class="form-text">
+            We will never share your email with anyone.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">
+            Password
+          </label>
+          <Field
+            validate={[requiredField]}
+            placeholder={"Password"}
+            name={"password"}
+            component={FormControl}
+            types="input"
+            type={"password"}
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="mb-3 form-check">
+          <Field
+            validate={[]}
+            placeholder={"Email"}
+            name={"email"}
+            component={FormControl}
+            types="input"
+            type="checkbox"
+            class="form-check-input"
+            id="exampleCheck1"
+          />
+
+          <label class="form-check-label" for="exampleCheck1">
+            Remember me
+          </label>
+        </div>
+
+        {captchaUrl && <img src={captchaUrl} alt="captcha" />}
+        {captchaUrl && (
+          <div className="pt-1 pb-1">
+            <Field
+              validate={[requiredField]}
+              placeholder={"Symbols from image"}
+              name={"captcha"}
+              component={FormControl}
+              types="input"
+              class="form-control"
+              id="exampleInput1"
+            />
+          </div>
+        )}
+        {error && <div className={"alert alert-danger"}>{error}</div>}
+        <button type="submit" class="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
   );
 };
 
@@ -68,7 +97,6 @@ const Login = (props) => {
   if (props.isAuth) return <Navigate to={"/profile"} />;
   return (
     <div>
-      <h1>LOGIN</h1>;
       <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
     </div>
   );
